8856f333da765534d6ef2a80e19fa20b61c9bb2b
RocketSmash <118481037+RocketSmash9000@users.noreply.github.com>
2024-11-26 22:45:54 +0100

    Added script and directories
    Added a script that toggles fullscreen and all the directories where all the things will be going

---
+++ Scripts/Main.gd
@@ -0,0 +1,16 @@
+extends Node2D
+# Define variables under this comment and above func _ready()
+
+
+# Called every time the node gets loaded into a scene.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_fullscreen"): # Toggles fullscreen
+		if DisplayServer.window_get_mode() == 3: # 3 = fullscreen enabled
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else: # If it's not 3, enable fullscreen
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
