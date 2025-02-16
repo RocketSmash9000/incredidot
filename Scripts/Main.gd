d54c154f0596cac937d5669da437f2f9e034aa3e
RocketSmash <118481037+RocketSmash9000@users.noreply.github.com>
2025-02-16 12:46:11 +0100

    Added reactive background
    Now the background will react to the beat as it plays. I honestly don't know how bass will work with the shading, but that's for you to figure out (I think)

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -10,6 +10,15 @@ var once = true # used to trigger stuff only once. Yk, code executing every fram
 
 var first_polo = true # True when there are no polos picked
 
+# The constants and variables below are used for shading purposes. Do not touch.
+@onready var background = $"Static elements/Background"
+
+# Change this if you want your background to react more aggresively
+var brightness_sensitivity: float = 1.0
+
+# Higher values will (probably) result in faster speeds when returning to original value
+var smoothing_speed: float = 5.0
+
 # Called every time the node gets loaded into a scene.
 func _ready() -> void:
 	# Sets the wait time of the timer to the duration of a loop
@@ -78,6 +87,15 @@ func _process(delta: float) -> void:
 	
 	if !first_polo and GlobalVars.picked_polos.is_empty():
 		first_polo = true # Sets this variable to true when there are no picked polos
+	
+	# The code below is used to make the background reactive to what is currently playing.
+	# If you don't know what you're doing, don't touch it.
+	var amplitude: float = get_music_amplitude() * brightness_sensitivity
+	amplitude = clamp(amplitude, 0.0, 1.0)
+	
+	var current_brightness: float = background.material.get_shader_parameter("brightness")
+	var new_brightness: float = lerp(current_brightness, amplitude, smoothing_speed * delta)
+	background.material.set_shader_parameter("brightness", new_brightness)
 
 # These need no explanation whatsoever, I think
 func _when_menu_icon_pressed() -> void:
@@ -116,3 +134,25 @@ func toggle_progressbar_visibility(visibility):
 		$"Static elements/ProgressCover".visible = visibility
 		$"Static elements/ProgressCover2".visible = visibility
 		$"Static elements/BaseProgressBar".visible = visibility
+
+func get_music_amplitude() -> float:
+	# All these variables have been assigned a type beforehand.
+	# This way they will not get assigned a different type and break.
+	
+	# Get the index of the "Master" audio bus.
+	var master_bus_index: int = AudioServer.get_bus_index("Master")
+	# The spectrum analyzer effect is at slot 0 on the Master bus.
+	var spectrum_analyzer = AudioServer.get_bus_effect_instance(master_bus_index, 0)
+	
+	if spectrum_analyzer:
+		# Get the magnitude (in dB) for a frequency range where the beat is prominent.
+		# Typical beat frequencies might be between 20 Hz and 150 Hz.
+		var magnitude_vec: Vector2 = spectrum_analyzer.get_magnitude_for_frequency_range(20.0, 150.0)
+		var magnitude_db: float = magnitude_vec.x * 2
+		# The returned magnitude is in dB.
+		# Normalize it to a 0 – 0.5 range for best results.
+		var normalized: float = clamp((magnitude_db), 0.0, 0.5)
+		return normalized
+	else:
+		# If for some reason the analyzer is not available, return 0.
+		return 0.0
